aa259be14438563ce7032c8a754ccd765ff7e49b
blinkofaneye <mratiarison41@gmail.com>
2020-01-23 15:39:23 +0300

    Add courses-top-menu.json

--- courses-site/src/pages/PageFive/components/TopMenu.js
+++ courses-site/src/pages/PageFive/components/TopMenu.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { makeStyles } from "@material-ui/core/styles";
 import Box from "@material-ui/core/Box";
 import Button from '@material-ui/core/Button';
+import axios from "axios";
 
 const useStyles = makeStyles(theme => ({
     root: {
@@ -11,15 +12,17 @@ const useStyles = makeStyles(theme => ({
 }));
 const TopMenu = () => {
     const classes = useStyles();
+    const [menu, setMenu] = useState([]);
+
+    useEffect(() => {
+      axios.get("/json/courses-top-menu.json")
+           .then(({data}) => setMenu(data));
+    }, []);
+
     return (<Box className={classes.root}>
-        <Button>VMware Cloud</Button>
-        <Button>Products</Button>
-        <Button>Support</Button>
-        <Button>Solutions</Button>
-        <Button>Professional Services</Button>
-        <Button>Downloads</Button>
-        <Button>Partners</Button>
-        <Button>Company</Button>
+      {menu.map(item => (
+        <Button>{item}</Button>
+      ))}
     </Box>);
 }
 
