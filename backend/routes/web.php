32d6af58c540e39e5174825429048cfa24869914
blinkofaneye <mratiarison41@gmail.com>
2020-01-25 08:12:37 +0300

    Add backend routes

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -13,3 +13,22 @@ Route::post("/add-to-cart/{id}", "MainSiteController@addToCart");
 
 // email-builder routes
 Route::view("/email-builder", "email-builder");
+
+// courses-site routes
+Route::view("/courses", "courses-site");
+Route::view("/courses/page-two", "courses-site");
+Route::view("/courses/page-Five", "courses-site");
+
+// teacher-panel routes
+Route::view("/teacher-panel", "teacher-panel");
+Route::view("/teacher-panel/sales", "teacher-panel");
+
+// react-dashboard routes
+Route::view("/admin/react/dashboard", "react-dashboard");
+Route::view("/admin/react/user", "react-dashboard");
+Route::view("/admin/react/table", "react-dashboard");
+Route::view("/admin/react/typography", "react-dashboard");
+Route::view("/admin/react/icons", "react-dashboard");
+Route::view("/admin/react/maps", "react-dashboard");
+Route::view("/admin/react/notifications", "react-dashboard");
+Route::view("/admin/react/upgrade-to-pro", "react-dashboard");
